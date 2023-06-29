10eadeb1ba6721b6ca84e2c0399829785cf0d614
zhangjing <zhangjing@wepie.com>
2023-06-29 16:30:59 +0800

    feat：添加旋转太极图View

--- module_home/src/main/java/com/example/modulehome/homePage/HomePageActivity.kt
+++ module_home/src/main/java/com/example/modulehome/homePage/HomePageActivity.kt
@@ -1,9 +1,12 @@
 package com.example.modulehome.homePage
 
+import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import com.example.libbase.ktx.observeLiveData
 import com.example.libcommon.baseui.BaseActivity
 import com.example.modulehome.databinding.HomePageActivityBinding
+import com.example.modulehome.talchi.TaiChiView
 import dagger.hilt.android.AndroidEntryPoint
 
 /****
@@ -11,9 +14,20 @@ import dagger.hilt.android.AndroidEntryPoint
  * @date : 星期二 2023/1/10
  */
 @AndroidEntryPoint
-class HomePageActivity: BaseActivity<HomePageActivityBinding, HomePageViewModel>() {
+class HomePageActivity : BaseActivity<HomePageActivityBinding, HomePageViewModel>() {
 
     override val viewModel by viewModels<HomePageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBinding.helloTitle.setOnClickListener {
+
+            Log.d(TaiChiView.TAG, "click")
+
+            viewBinding.taiChiView.animPlay()
+        }
+    }
 
     override fun HomePageActivityBinding.initView() {
 

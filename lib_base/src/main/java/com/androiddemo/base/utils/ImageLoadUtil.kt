a835e965708f0b1380b5c3c64006efa09c521d60
zhangjing <zhangjing@wepie.com>
2023-10-07 11:52:22 +0800

    involve BigImageView

--- lib_base/src/main/java/com/androiddemo/base/utils/ImageLoadUtil.kt
+++ lib_base/src/main/java/com/androiddemo/base/utils/ImageLoadUtil.kt
@@ -1,7 +1,14 @@
 package com.androiddemo.base.utils
 
+import android.content.Context
 import android.widget.ImageView
+import com.androiddemo.base.BaseApplication
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import java.io.File
 
 /****
  * @author : zhangjing
@@ -29,6 +36,35 @@ object ImageLoadUtil {
             .centerCrop()
             .into(imageView)
     }
+
+    fun downloadOnly(url: String, downloadListener: ImageDownloadListener) {
+        downloadOnly(BaseApplication.context, url, downloadListener)
+    }
+
+    fun downloadOnly(context: Context, url: String, downloadListener: ImageDownloadListener) {
+        Glide.with(context).download(url).addListener(
+            object : RequestListener<File> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<File>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: File,
+                    model: Any,
+                    target: Target<File>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            }
+        ).submit()
+    }
 }
 
 class ImageLoadConfig {
@@ -36,3 +72,9 @@ class ImageLoadConfig {
 }
 
 interface ImageLoadListener {}
+
+interface ImageDownloadListener {
+    fun onLoadFailed(e: Exception?, model: String)
+
+    fun onResourceReady(resource: File)
+}
--- lib_common/src/main/java/com/androiddemo/common/ui/imageinspect/ImageInspectAdapter.kt
+++ lib_common/src/main/java/com/androiddemo/common/ui/imageinspect/ImageInspectAdapter.kt
@@ -1,6 +1,7 @@
 package com.androiddemo.common.ui.imageinspect
 
 import android.annotation.SuppressLint
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +11,13 @@ import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.adapter.FragmentViewHolder
+import com.androiddemo.base.utils.ImageDownloadListener
 import com.androiddemo.base.utils.ImageLoadUtil
 import com.androiddemo.common.R
+import com.github.piasy.biv.view.BigImageView
+import com.shizhefei.view.largeimage.LargeImageView
+import com.shizhefei.view.largeimage.factory.FileBitmapDecoderFactory
+import java.io.File
 
 /****
  * @author : zhangjing
@@ -47,8 +53,22 @@ class ImageInspectAdapter :
     class ImageInspectViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val normalImage: ImageView = itemView.findViewById(R.id.normal_image)
+        private val bigImage: BigImageView = itemView.findViewById(R.id.big_image)
+        private val largeImage: LargeImageView = itemView.findViewById(R.id.large_image)
         fun bind(imageUrl: String) {
-            ImageLoadUtil.load(imageUrl, normalImage)
+//            ImageLoadUtil.load(imageUrl, normalImage)
+            bigImage.showImage(Uri.parse(imageUrl))
+//            largeImage.isEnabled = true
+//            ImageLoadUtil.downloadOnly(itemView.context, imageUrl, object : ImageDownloadListener {
+//                override fun onLoadFailed(e: Exception?, model: String) {
+//
+//
+//                }
+//
+//                override fun onResourceReady(resource: File) {
+//                    largeImage.setImage(FileBitmapDecoderFactory(resource))
+//                }
+//            })
         }
     }
 }
